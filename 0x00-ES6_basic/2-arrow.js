6e72b4ba16e051e5a0ecd404b6b049f22cc48582
laamyry <slamiri10@gmail.com>
2024-01-03 14:55:50 +0100

    update task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,10 @@
 export default function getNeighborhoodsList () {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square']
-
+  /* eslint-disable no-unused-vars */
   const self = this
   const add = (this.addNeighborhood = newNeighborhood => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood)
     return self.sanFranciscoNeighborhoods
   })
+  /* eslint-disable no-unused-vars */
 }
