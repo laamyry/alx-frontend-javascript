9231c896b51f368b7f24ad3211e11fa1d58716b6
laamyry <slamiri10@gmail.com>
2024-01-03 21:39:41 +0100

    add task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const output = [];
+  for (const value of array) {
+    output.push(`${appendString}${value}`);
+  }
+
+  return output;
+}
