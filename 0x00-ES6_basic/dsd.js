2be02d2ede5c45370fc902d7d214826e7c632388
laamyry <slamiri10@gmail.com>
2024-01-03 21:11:40 +0100

    update

--- 0x00-ES6_basic/dsd.js
+++ 0x00-ES6_basic/dsd.js
@@ -1,3 +1,12 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  return initialNumber + expansion1989 + expansion2019;
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
 }
