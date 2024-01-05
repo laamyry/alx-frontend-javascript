c2009af2efcfb39c2ab2b3ce09b4c5c57258315e
laamyry <slamiri10@gmail.com>
2024-01-06 00:10:35 +0100

    add task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function getFullResponseFromAPI(success) {
+  let output = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return output;
+}
