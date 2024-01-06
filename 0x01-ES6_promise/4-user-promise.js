8fb07944ae602326a10566343a2f15308ec6016e
laamyry <slamiri10@gmail.com>
2024-01-06 01:34:36 +0100

    update taks 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
 export default function signUpUser(firstName, lastName) {
-    const output = Promise.resolve({ firstName, lastName });
-    return output
-  }
+  const output = Promise.resolve({ firstName, lastName });
+  return output;
+}
