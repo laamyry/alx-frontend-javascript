9a885e5a87c86494fe81ebcadf7a68a524425899
laamyry <slamiri10@gmail.com>
2024-01-08 20:35:45 +0100

    add task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,7 @@
-export default function getListStudentIds() {
-    
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  } else {
+    return array.map((el) => el.id);
+  }
 }
