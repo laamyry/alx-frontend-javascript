143fdf19cadb93ec1f2840bdc935aedfa5c9939d
laamyry <slamiri10@gmail.com>
2024-01-09 02:26:59 +0100

    add task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  return array
+    .filter((el) => el.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((el) => el.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return { ...person, grade };
+    });
+}
