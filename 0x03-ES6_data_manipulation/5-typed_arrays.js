9453b3da142759b2fdc0c6486250f61c18e340e4
laamyry <slamiri10@gmail.com>
2024-01-10 01:43:03 +0100

    add tasks

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position >= length) throw Error('Position outside range');
-    const buffer = new ArrayBuffer(length);
-    const newView = new DataView(buffer, 0, length);
-    const arr = new Int8Array(buffer);
-    arr[position] = value;
-    return newView;
+  if (position >= length) {
+    throw new Error('Position outside range');
   }
+  const Int8 = new Int8Array(length);
+  Int8.fill(value, position, position + 1);
+  return new DataView(Int8.buffer);
+}
---
+++ 0x03-ES6_data_manipulation/test.js
@@ -0,0 +1,11 @@
+// const SETTING = {
+//     'name' : 'Med',
+//     'age' : 26
+// };
+// // Object.freeze(SETTING);
+
+// SETTING.name = 'Mohamed';
+// console.log(SETTING);
+let reg = (param) => param * 2;
+console.log(reg(5));
+
